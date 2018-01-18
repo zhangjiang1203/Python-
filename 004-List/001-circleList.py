7cd42601213d42fece76c3ec3a1f447759204719
zhangjiang <zhangjiang@shanlinjinrong.com>
2018-01-18 17:34:46 +0800

    修改了文件中的输出

--- 004-List/001-circleList.py
+++ 004-List/001-circleList.py
@@ -1,3 +1,7 @@
 magicians = ['dawei','lilin','wangdan']
 for magician in  magicians:
-    print(magician)
+    print(magician.title()+',that was a greate trick')
+    print("I cant‘t wati to see your next trick, " + magician.title()+"\n")
+
+
+print("Thank you ,everyone. That was a great magic show!")
