fa24e423a2fac94b30ae204aa43dd67a41de7810
张江 <896884553@qq.com>
2018-06-25 22:24:43 +0800

    修改显示的作业

--- 作业/003-zuoye.py
+++ 作业/003-zuoye.py
@@ -1,60 +1,66 @@
 import os
 
-file_name = 'account.txt'
+
 def login():
     flag = True
-    users = getUseraccount()
-    print(users)
     while flag:
         # 用户是否存在
+        users = getUseraccount()
+        for user in users:
+            print(user)
         isexist = False
         account = input('请输入您的账号:')
+        psw = input("请输入您的密码:")
         for user in users:
             if account in user.values():
                 isexist = True
-                flag = user['loginCount']
+                flag = int(user['loginCount'])
                 if flag == 3:
                     print('您登录密码输入错误次数过多，账号已被锁定')
                     flag = False
                     break
                 #拿到对应的password和count值
-                psw = input("请输入您的密码:")
                 if psw == user['password']:
                     print('登录成功')
+                    #重置用户登录次数
+                    users.remove(user)
+                    user['loginCount'] = 0
+                    users.append(user)
+                    changAllAccount(users)
                     flag = False
-                    #开始下次循环
-                    continue
+                    break
                 else:
                     flag+=1
+                    #修改用户信息
+                    users.remove(user)
+                    user['loginCount'] = flag
+                    users.append(user)
+                    changAllAccount(users)
                     if flag == 3:
                         print('密码输入错误3次,账号已被锁定')
-                        #修改用户的信息
-                        users.remove(user)
-                        # tempuser = users[:]
-                        user['loginCount'] = flag
-                        users.append(user)
-                        changAllAccount(users)
                         flag = False
                         break
                     else:
                         print('密码输入错误%s次,请重试' %flag)
+                        break;
+
 
         if not isexist:
             print('用户不存在，请重试')
 
-
 #编辑用户
+file_name = 'account.txt'
 def getUseraccount():
     #文件是否存在
     if os.path.isfile(file_name):
         with open(file_name, 'r+') as file_object:
             contents = file_object.readlines()
             users = []
-            # print(contents)
             for line in contents:
                 templine = line.strip()
                 if not len(templine) or line.startswith('#'):
                     continue
+                #字符串转字典
                 users.append(eval(templine));
         return users
     else:
