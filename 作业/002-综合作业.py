50393e326bb2ceda3f1b631ba525fa35867d5794
张江 <896884553@qq.com>
2018-07-03 02:03:37 +0800

    添加了部分作业

--- 作业/002-综合作业.py
+++ 作业/002-综合作业.py
@@ -21,14 +21,42 @@ def pyramid():
             print('*',end='') #在一行中连续打印多个*
         print()
 
-def shoplist():
-    markets = {
-        'apple': 10,
-        'tesla': 100000,
-        'mac': 3000,
-        'lenovo': 30000,
-        'chicken': 10,
-    }
+def shoplist(user):
+    product_list = [['Iphone7', 5800],
+                    ['Coffee', 30],
+                    ['疙瘩汤', 10],
+                    ['Python Book', 99],
+                    ['Bike', 199],
+                    ['ViVo X9', 2499]]
+    shops_mark = {}
+    tag = True
+    while tag:
+        for index,value in enumerate(product_list):
+            print(index,value)
+        market = input("输入商品编号选择商品，q退出>>:").strip()
+        if market == 'q':
+            #输出所有购买的商品，并确认是否购买
+        print(shops_mark)
+        if market.isdigit():
+            index = int(market)
+            if index < 0 or index >= len(product_list):
+                continue
+            balance = user['salary']
+            product_name = product_list[index][0]
+            product_price = product_list[index][1]
+            if balance > product_price:
+                # 添加到购物车中
+                if product_name in shops_mark:
+                    shops_mark[product_name]['count']+=1
+                else:
+                    shops_mark[product_name] = {"price":product_price,'count':1}
+            else:
+                print('对不起客官，您的余额不足，原价 {} 请充值{}'.format(product_price,product_price-balance))
+            print(shops_mark)
+        else:
+            print('输入不合法')
+
+
 
 #编辑用户
 file_name = 'account.txt'
@@ -54,13 +82,19 @@ def login():
                 #拿到对应的password和count值
                 if psw == user['password']:
                     print('登录成功')
-                    #重置用户登录次数
+                    #输入工资，接下来选择商品
+                    salary = input('请输入您的工资>>:').strip()
+                    #重置用户登录次数并保存工资到对应的账号中
                     users.remove(user)
                     user['loginCount'] = 0
+                    user['salary'] = int(salary)
                     users.append(user)
                     changAllAccount(users)
                     flag = False
-                    break
+                    #输出商品列表
+
+
+
                 else:
                     flag+=1
                     #修改用户信息
@@ -74,7 +108,7 @@ def login():
                         break
                     else:
                         print('密码输入错误%s次,请重试' %flag)
-                        break;
+                        break
         if not isexist:
             print('用户不存在，请重试')
 
@@ -93,6 +127,7 @@ def getUseraccount():
                 users.append(eval(templine));
         return users
     else:
+        # 文件不存在就创造文件
         with open(file_name, 'a+') as object:
             users = [{'account': 'zhang', 'password': '123456', 'loginCount': 2,'salary':0},
                      {'account': 'wang', 'password': '123456', 'loginCount': 0,'salary':0},
@@ -123,13 +158,17 @@ while tag:
     choice = input('>>:').strip()
     if choice == 'q' or choice == 'Q':
         break
-    choice = int(choice)
-    if choice == 1:
-        multiplicationtable()
-    elif choice == 2:
-        pyramid()
+    if choice.isdigit():
+        choice = int(choice)
+        if choice == 1:
+            multiplicationtable()
+        elif choice == 2:
+            pyramid()
+        else:
+            shoplist()
+            # login()
     else:
-        login()
+        print('输入不合法，请输入数字')
 
 
 
