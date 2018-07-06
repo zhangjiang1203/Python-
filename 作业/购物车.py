e5e87ad496ef5519c32638d42a00bd1c279718cf
zhangjiang1203 <896884553@qq.com>
2018-07-06 17:36:41 +0800

    修改文件

--- 作业/购物车.py
+++ 作业/购物车.py
@@ -1,1 +1,15 @@
-import os
+def functionNmae(func):
+    def wrapper(*args,**kwargs):
+        print('我就是你')
+        res = func(*args,**kwargs)
+        return res
+
+    return wrapper
+
+
+@functionNmae
+def show():
+    print('我就是我')
+
+
+show()
