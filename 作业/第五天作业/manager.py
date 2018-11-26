58d7af38e1c664d82a716e4fa4dbc4893fd06411
zhangjiang <zhangjiang@imcoming.cn>
2018-11-26 14:31:17 +0800

    添加项目列表

---
+++ 作业/第五天作业/manager.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/26 2:22 PM
+# @Author  : zhangjiang
+# @Site    : 
+# @File    : manager.py
+# @Software: PyCharm
+
+
+if __name__ == "__main__":
+    
